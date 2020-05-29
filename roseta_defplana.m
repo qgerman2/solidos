f97904f5c90a05ac38be643c2f3340d36eb488f1
qgerman2 <qgerman2@gmail.com>
2020-05-28 21:31:00 -0400

    espero no haber roto el codigo de las rosetas

--- roseta_defplana.m
+++ roseta_defplana.m
@@ -1,25 +1,19 @@
 %angulos en grados, deformaciones en microstrain, young en mpa
 function [e, o] = roseta_defplana(a1, a2, a3, e1, e2, e3, young, poisson)
-    %grados a radian
-    a1 = deg2rad(a1);
-    a2 = deg2rad(a2);
-    a3 = deg2rad(a3);
-    %encontrar deformacion xx, yy e xy
-    syms exx eyy exy
-    eqn1 = (exx + eyy) / 2 + (exx - eyy) / 2 * cos(2 * a1) + exy * sin(2 * a1) == e1;
-    eqn2 = (exx + eyy) / 2 + (exx - eyy) / 2 * cos(2 * a2) + exy * sin(2 * a2) == e2;
-    eqn3 = (exx + eyy) / 2 + (exx - eyy) / 2 * cos(2 * a3) + exy * sin(2 * a3) == e3;
-    S = vpasolve(eqn1, eqn2, eqn3);
+    S = roseta(a1, a2, a3, e1, e2, e3);
+    exx = S(1,1);
+    eyy = S(2,2);
+    exy = S(1,2);
     %parametros de lame
     lambda = (young * poisson) / ((1 + poisson) * (1 - 2 * poisson));
     u = young / (2 * (1 + poisson));
     %ecuaciones para encontrar esfuerzo a partir de deformacion plana
-    oxx = (lambda + 2 * u) * S.exx * (10 ^ -4) + lambda * S.eyy * (10 ^ -4);
-    oyy = lambda * S.exx * (10 ^ -4) + (lambda + 2 * u) * S.eyy * (10 ^ -4);
-    oxy = u * 2 * S.exy * (10 ^ -4);
-    ozz = lambda * S.exx * (10 ^ -4) + lambda * S.eyy * (10 ^ -4);
-    e = [S.exx, S.exy, 0;
-        S.exy, S.eyy, 0;
+    oxx = (lambda + 2 * u) * exx * (10 ^ -6) + lambda * eyy * (10 ^ -6);
+    oyy = lambda * exx * (10 ^ -6) + (lambda + 2 * u) * eyy * (10 ^ -6);
+    oxy = u * 2 * exy * (10 ^ -6);
+    ozz = lambda * exx * (10 ^ -6) + lambda * eyy * (10 ^ -6);
+    e = [exx, exy, 0;
+        exy, eyy, 0;
         0, 0, 0];
     o = [oxx, oxy, 0;
         oxy, oyy, 0;
--- roseta_esfplano.m
+++ roseta_esfplano.m
@@ -1,23 +1,17 @@
 %angulos en grados, deformaciones en microstrain, young en mpa
 function [e, o] = roseta_esfplano(a1, a2, a3, e1, e2, e3, young, poisson)
-    %grados a radian
-    a1 = deg2rad(a1);
-    a2 = deg2rad(a2);
-    a3 = deg2rad(a3);
-    %encontrar deformacion xx, yy e xy
-    syms exx eyy exy
-    eqn1 = (exx + eyy) / 2 + (exx - eyy) / 2 * cos(2 * a1) + exy * sin(2 * a1) == e1;
-    eqn2 = (exx + eyy) / 2 + (exx - eyy) / 2 * cos(2 * a2) + exy * sin(2 * a2) == e2;
-    eqn3 = (exx + eyy) / 2 + (exx - eyy) / 2 * cos(2 * a3) + exy * sin(2 * a3) == e3;
-    S = vpasolve(eqn1, eqn2, eqn3);
+    S = roseta(a1, a2, a3, e1, e2, e3);
+    exx = S(1,1);
+    eyy = S(2,2);
+    exy = S(1,2);
     %ecuaciones para encontrar esfuerzo plano a partir de deformacion
-    oxx = (young / (1 - poisson^2)) * (S.exx * (10 ^ -6) + poisson * S.eyy * (10 ^ -6));
-    oyy = (young / (1 - poisson^2)) * (poisson * S.exx * (10 ^ -6) + S.eyy * (10 ^ -6));
-    oxy = (young / (1 - poisson^2)) * (S.exy * (10 ^ -6) * (1 - poisson));
+    oxx = (young / (1 - poisson^2)) * (exx * (10 ^ -6) + poisson * eyy * (10 ^ -6));
+    oyy = (young / (1 - poisson^2)) * (poisson * exx * (10 ^ -6) + eyy * (10 ^ -6));
+    oxy = (young / (1 - poisson^2)) * (exy * (10 ^ -6) * (1 - poisson));
     %deformacion zz a partir de esfuerzos oxx y oyy (esfuerzo plano)
     ezz = (1 / young) * (-poisson * (oxx + oyy)) * (10 ^ 6);
-    e = [S.exx, S.exy, 0;
-        S.exy, S.eyy, 0;
+    e = [exx, exy, 0;
+        exy, eyy, 0;
         0, 0, ezz];
     o = [oxx, oxy, 0;
         oxy, oyy, 0;
