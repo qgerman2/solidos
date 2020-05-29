e4508813601ee25155a713da719da931b2f20812
qgerman2 <qgerman2@gmail.com>
2020-05-28 21:36:17 -0400

    tamo

--- MS18Ejercicios_1b.m
+++ MS18Ejercicios_1b.m
@@ -2,14 +2,17 @@
 %calcular tensores esfuerzo y deformacion enteros con roseta y condicion de
 %esfuerzo plano
 [e, o] = roseta_esfplano(30, 75, 120, 10, 5, 1, 210000, 0.27);
-disp(o)
+fprintf('Tensor esfuerzo\n')
+disp(o);
 %angulos
 [o1, o2] = direcciones_principales(o);
+fprintf('Direcciones principales y direccion de corte maximo\n');
 disp(o1);
-disp(o2)
+disp(o2);
 angulo_corte_0 = o1 + 90;
 angulo_corte_maximo = angulo_corte_0 + 45;
-disp(angulo_corte_maximo)
+disp(angulo_corte_maximo);
 %rotar el esfuerzo por el angulo con corte maximo
 o_corte_maximo = rotar_plano(o, angulo_corte_maximo);
-disp(o_corte_maximo)
+fprintf('Tensor esfuerzo corte maximo\n')
+disp(o_corte_maximo);
